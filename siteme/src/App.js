ad8a304124062edcfd1e1150ddf917634ae3ca74
Aliaghaei1991 <aliaghaeii70@gmail.com>
2024-01-05 21:16:13 +0330

    first commit

---
+++ siteme/src/App.js
@@ -0,0 +1,30 @@
+import Header from "./pages/Header";
+import Body from "./pages/Body";
+
+import '../node_modules/bootstrap/dist/css/bootstrap.rtl.min.css'
+
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+function App() {
+  return (
+
+    <div dir="rtl" className="container">
+
+      <Router>
+        <Routes>
+          <Route action={"/Header"} element={<Header />} />
+        </Routes>
+      </Router>
+      
+
+      <Header />
+      <Body />
+
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ siteme/src/pages/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar_ from './Navbar_';
+
+
+const Header = () => {
+
+
+    return (
+        <div>
+
+            <Navbar_ />
+
+
+        </div>
+    )
+}
+
+
+export default Header;
